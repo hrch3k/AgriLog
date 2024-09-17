17aa79f6ec00f53eeeeca1d6e1bad17b600790ed
4ndr3j <andrej.jurkas@dia-vit.com>
2024-09-17 23:17:18 +0200

    Separated home_screen from main.dart and added auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
-import 'data_service.dart'; // Import DataService
-import 'details_screen.dart';
-import 'database_helper.dart';
-import 'add_field_screen.dart';
-import 'package:intl/intl.dart';
-import 'export_screen.dart';
-import 'package:firebase_core/firebase_core.dart'; // Import Firebase core
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'home_screen.dart'; // Your main content screen
+import 'sign_in_screen.dart'; // Your sign-in screen
+import 'package:firebase_auth/firebase_auth.dart'; // Firebase authentication
+import 'package:flutter_dotenv/flutter_dotenv.dart'; // For loading environment variables
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); // Initialize Firebase
-  print('Firebase Initialized Successfully');
+
+  // Load environment variables from the .env file
+  await dotenv.load(fileName: "assets/.env");
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(MyApp());
 }
 
@@ -19,390 +25,43 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'AgriLog',
-      home: DataScreen(),
+      home: AuthWrapper(), // Use AuthWrapper to handle login state
     );
   }
 }
 
-class DataScreen extends StatefulWidget {
-  @override
-  _DataScreenState createState() => _DataScreenState();
-}
-
-class _DataScreenState extends State<DataScreen> {
-  final DataService _dataService = DataService();
-  final TextEditingController _controller = TextEditingController();
-  List<Map<String, dynamic>> _data = [];
-  bool _isLoading = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadLastKmgMidData(); // Load parcels for the last added kmg_mid on startup
-  }
-
-  Future<void> _loadLastKmgMidData() async {
-    DatabaseHelper dbHelper = DatabaseHelper();
-
-    // Fetch the last kmg_mid
-    int? lastKmgMid = await dbHelper.getLastKmgMid();
-
-    if (lastKmgMid != null) {
-      _controller.text =
-          lastKmgMid.toString(); // Update the controller with the last kmg_mid
-      _loadDataFromDatabase(lastKmgMid);
-    }
-  }
-
-  Future<void> _loadDataFromDatabase(int inputNumber) async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    await _loadData(inputNumber);
-
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (_controller.text.isNotEmpty) {
-      final inputNumber = int.tryParse(_controller.text);
-      if (inputNumber != null) {
-        _loadDataFromDatabase(inputNumber);
-      }
-    }
-  }
-
-  Future<void> _fetchData(int inputNumber) async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    await _dataService.fetchDataAndSaveToDatabase(inputNumber);
-    await _loadData(inputNumber);
-
-    setState(() {
-      _isLoading = false;
-    });
-  }
-
-  Future<void> _loadData(int inputNumber) async {
-    List<Map<String, dynamic>> data =
-        await _dataService.getParcelsForKmgMid(inputNumber);
-    setState(() {
-      _data = data;
-    });
-  }
-
-  Future<Map<String, dynamic>?> _getLastJobForParcel(int parcelId) async {
-    DatabaseHelper dbHelper = DatabaseHelper();
-    return await dbHelper.getLastJobForParcel(parcelId);
-  }
-
-  Future<void> _deleteParcel(int parcelId) async {
-    DatabaseHelper dbHelper = DatabaseHelper();
-    await dbHelper.deleteParcel(parcelId);
-    setState(() {
-      // Reload the data after deletion
-      final inputNumber = int.tryParse(_controller.text);
-      if (inputNumber != null) {
-        _loadDataFromDatabase(inputNumber);
-      }
-    });
-  }
-
-  Color _getFieldColor(String? detailType) {
-    switch (detailType) {
-      case 'Priprava Zemlje':
-        return Colors.brown;
-      case 'Setev':
-        return Colors.green;
-      case 'Škropljenje':
-        return Colors.blue;
-      case 'Dognojevanje':
-        return Colors.orange;
-      case 'Baliranje':
-        return Colors.yellow;
-      case 'Žetev':
-        return Colors.red;
-      default:
-        return Colors.grey;
-    }
-  }
-
-  void _onSelectedMenuOption(String option) {
-    switch (option) {
-      case 'Izvozi Podatke':
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => ExportScreen(),
-          ),
-        );
-        break;
-    }
-  }
-
+class AuthWrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    List<Map<String, dynamic>> gerkItems =
-        _data.where((item) => item['is_gerk'] == 1).toList();
-    List<Map<String, dynamic>> nonGerkItems =
-        _data.where((item) => item['is_gerk'] != 1).toList();
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      // Listen to auth state changes
+      builder: (context, snapshot) {
+        // Debugging: Print whenever the auth state changes
+        print('User state changed: ${snapshot.data}');
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('AgriLog'),
-        actions: [
-          PopupMenuButton<String>(
-            onSelected: _onSelectedMenuOption,
-            itemBuilder: (BuildContext context) {
-              return {'Izvozi Podatke'}.map((String choice) {
-                return PopupMenuItem<String>(
-                  value: choice,
-                  child: Text(choice),
-                );
-              }).toList();
-            },
-            icon: Icon(Icons.settings),
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Vnesi KMG-MID',
-                    ),
-                  ),
-                ),
-                SizedBox(width: 10),
-                SizedBox(
-                  width: 100,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      final inputNumber = int.tryParse(_controller.text);
-                      if (inputNumber != null) {
-                        _fetchData(inputNumber);
-                      }
-                    },
-                    child: Text('Pridobi GERK-e'),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            _isLoading
-                ? CircularProgressIndicator()
-                : Expanded(
-                    child: ListView(
-                      children: [
-                        if (gerkItems.isNotEmpty) ...[
-                          _buildItemsContainer(
-                              items: gerkItems,
-                              title: 'GERK',
-                              borderColor: Colors.green),
-                        ],
-                        if (nonGerkItems.isNotEmpty) ...[
-                          _buildItemsContainer(
-                              items: nonGerkItems,
-                              title: 'Brez GERK-a',
-                              borderColor: Colors.black),
-                        ],
-                      ],
-                    ),
-                  ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context)
-              .push(
-            MaterialPageRoute(
-              builder: (context) => NewFieldScreen(),
-            ),
-          )
-              .then((_) {
-            setState(() {
-              final inputNumber = int.tryParse(_controller.text);
-              if (inputNumber != null) {
-                _loadDataFromDatabase(inputNumber);
-              }
-            });
-          });
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show loading indicator while Firebase is connecting
+          return Center(child: CircularProgressIndicator());
+        }
 
-  Widget _buildItemsContainer({
-    required List<Map<String, dynamic>> items,
-    required String title,
-    required Color borderColor,
-  }) {
-    return Container(
-      margin: EdgeInsets.only(bottom: 20.0),
-      padding: EdgeInsets.all(10.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: borderColor, width: 2),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: borderColor,
-            ),
-          ),
-          SizedBox(height: 10),
-          ...items.map((item) {
-            return FutureBuilder<Map<String, dynamic>?>(
-              future: _getLastJobForParcel(item['id']),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 4.0),
-                    height: 100.0,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[300],
-                      border: Border.all(color: Colors.black),
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Center(
-                      child: CircularProgressIndicator(),
-                    ),
-                  );
-                }
+        if (snapshot.hasData) {
+          User? user = snapshot.data;
 
-                if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                }
+          // Check if we're in development mode, and optionally skip email verification
+          bool isDev = dotenv.env['IS_DEV'] == 'true';
 
-                var lastJob = snapshot.data;
-                String detailType = lastJob?['detail_type'] ?? 'No job data';
-                String jobType = lastJob?['job_type'] ?? 'Unknown job type';
+          // If the user is logged in and either verified or we're in dev mode
+          if (user != null && (user.emailVerified || isDev)) {
+            return DataScreen(); // Go to your main content screen (e.g., DataScreen)
+          } else {
+            // User is logged in but hasn't verified their email
+            return SignInPage(showEmailNotVerified: true);
+          }
+        }
 
-                DateTime? parsedDateTime;
-                if (lastJob?['date_time'] != null) {
-                  parsedDateTime = DateTime.parse(lastJob!['date_time']);
-                }
-
-                String dateTime = parsedDateTime != null
-                    ? DateFormat('dd.MM.yyyy HH:mm').format(parsedDateTime)
-                    : 'Unknown date/time';
-                Color backgroundColor = _getFieldColor(detailType);
-
-                return Container(
-                  margin: EdgeInsets.symmetric(vertical: 4.0),
-                  decoration: BoxDecoration(
-                    color: backgroundColor,
-                    border: Border.all(color: Colors.black),
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                  child: ListTile(
-                    contentPadding: EdgeInsets.all(8.0),
-                    title: Text(
-                      item['domace_ime'],
-                      style: TextStyle(color: Colors.black),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Opravilo: $detailType',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        Text(
-                          'Tip: $jobType',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        Text(
-                          'Datum: $dateTime',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                        Text(
-                          'Površina: ${item['m2'] / 100} ar',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
-                    ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.close),
-                      color: Colors.black,
-                      onPressed: () async {
-                        bool? confirm = await _showDeleteConfirmation(context);
-                        if (confirm == true) {
-                          await _deleteParcel(item['id']);
-                        }
-                      },
-                    ),
-                    onTap: () {
-                      Navigator.of(context)
-                          .push(
-                        MaterialPageRoute(
-                          builder: (context) =>
-                              FieldDetailsScreen(fieldId: item['id']),
-                        ),
-                      )
-                          .then((dataChanged) {
-                        if (dataChanged == true) {
-                          final inputNumber = int.tryParse(_controller.text);
-                          if (inputNumber != null) {
-                            _loadDataFromDatabase(inputNumber);
-                          }
-                        }
-                      });
-                    },
-                  ),
-                );
-              },
-            );
-          }).toList(),
-        ],
-      ),
-    );
-  }
-
-  Future<bool?> _showDeleteConfirmation(BuildContext context) async {
-    return showDialog<bool>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Odstrani Parcelo'),
-          content: Text(
-              'Ali ste prepričani? Odstranjena bo vsa zgodovina dela za parcelo.'),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Prekliči'),
-              onPressed: () {
-                Navigator.of(context).pop(false);
-              },
-            ),
-            TextButton(
-              child: Text('Izbriši'),
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-            ),
-          ],
-        );
+        // If no user is logged in, show the sign-in screen
+        return SignInPage(showEmailNotVerified: false);
       },
     );
   }
