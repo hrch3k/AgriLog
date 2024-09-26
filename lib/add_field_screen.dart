d26790a8acb3ab5fdebc127f0ad23a50d268f4d6
4ndr3j <andrej.jurkas@dia-vit.com>
2024-09-26 23:26:13 +0200

    migrate sql to firebase

--- lib/add_field_screen.dart
+++ lib/add_field_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'database_helper.dart';
+import 'firebase_helper.dart'; // Import FirestoreHelper
 
 class NewFieldScreen extends StatefulWidget {
   @override
@@ -10,7 +10,8 @@ class _NewFieldScreenState extends State<NewFieldScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _typeController = TextEditingController();
   final TextEditingController _sizeController = TextEditingController();
-  final DatabaseHelper _dbHelper = DatabaseHelper();
+  final FirestoreHelper _firestoreHelper =
+      FirestoreHelper(); // FirestoreHelper instance
 
   @override
   Widget build(BuildContext context) {
@@ -50,11 +51,32 @@ class _NewFieldScreenState extends State<NewFieldScreen> {
                 String type = _typeController.text;
                 double? size = double.tryParse(_sizeController.text);
 
-                if (name.isNotEmpty && size != null) {
-                  await _dbHelper.insertField(name, size);
+                if (name.isNotEmpty && type.isNotEmpty && size != null) {
+                  // Get the user's current KMG MID
+                  int? kmgMidId = await _firestoreHelper.getLastKmgMid();
+                  if (kmgMidId == null) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('No active KMG MID found')),
+                    );
+                    return;
+                  }
+
+                  // Construct the new field (parcel) data
+                  Map<String, dynamic> parcelData = {
+                    'domace_ime': name,
+                    'type': type,
+                    'm2': size * 100, // Convert 'ar' to 'm²'
+                    'kmg_mid_id': kmgMidId,
+                    'created_at': DateTime.now().toIso8601String(),
+                  };
+
+                  // Save the parcel data to Firestore
+                  await _firestoreHelper.insertOrUpdateParcel(parcelData);
+
+                  // Navigate back after successful save
                   Navigator.of(context).pop();
                 } else {
-                  // Show an error message if any field is empty
+                  // Show an error message if any field is empty or invalid
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(content: Text('Please fill all fields correctly')),
                   );
